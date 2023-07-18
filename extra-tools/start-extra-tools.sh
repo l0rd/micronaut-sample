9cd59c1e1671a788b6f8ba5eb1e8d03bb65ee621
Mario Loriedo <mario.loriedo@gmail.com>
2023-07-18 12:20:08 +0000

    Retry using contributions for extra-tools

--- extra-tools/start-extra-tools.sh
+++ extra-tools/start-extra-tools.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+echo "#####################################"
+echo "Starting execution of the script ${0}"
+echo "#####################################"
+
+
 # Start kubedock
 if [ -n "$EXTRA_TOOLS_KUBEDOCK" ]; then
+    echo "Kubedock support enabled" 
     while [ ! -f /home/user/.kube/config ]; do 
         sleep 1 
     done
     /extra-tools/kubedock server --port-forward  > /extra-tools/kubedock.log 2>&1 &
+    echo "Kubedock started" 
+else
+    echo "Kubedock support disabled" 
 fi
 
 # Set Java version
@@ -15,3 +24,7 @@ if [ -n "$EXTRA_TOOLS_JAVA_VERSION" ]; then
 else
     echo "EXTRA_TOOLS_JAVA_VERSION not set" 
 fi
+
+echo "#####################################"
+echo "Execution completed"
+echo "#####################################"
